70e8df139e79d6469eff1dfb3f0e8f9ae9794206
viniorc <vorc.tec@gmail.com>
2023-06-22 21:52:59 -0300

    construcao da tabela com ronie

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
-const preencherTabela = () => {
+const buscarUsuarios = async() => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    const usuarios = await response.json();
 
-fetch('https://jsonplaceholder.typicode.com/users')
-.then(Response => Response.json())
-.then(json => {
-    //console.log(json);
-
-    
-
-    json.forEach(json => {
-        console.log(json);
-        
-    });
-
-
-})
+    return usuarios;
 }
+
+const usuarioToRowHTML = (usuario) => {
+
+    return`
+        <tr>
+            <td>${usuario.id}</td>
+            <td>${usuario.name}</td>
+            <td>${usuario.email}</td>
+            <td>${usuario.username}</td>
+            <td>${usuario.website}</td>
+        </tr>
+    `
+}
+const contruir_tabela = async () => {
+
+    const usuarios = await buscarUsuarios();
+    
+    const content = document.getElementById('tbody');
+
+    const usuariosHTML = usuarios.map(usuarioToRowHTML)
+
+    usuariosHTML.forEach((elementoHTML) => content.innerHTML += elementoHTML)
+    
+    }
+
+    contruir_tabela()
